3b0818af529af0e0b0ff92c41fee01d828b073ad
Nishant <nishantkumardash26@gmail.com>
2022-05-15 16:43:25 +0530

    second commit

--- IndividualThreads/Main.java
+++ IndividualThreads/Main.java
@@ -1,14 +1,31 @@
 package IndividualThreads;
+import java.util.ArrayList;
 public class Main {
     
 
-    public static void main(String args[])
+    public static void main(String args[]) 
     {
+        ArrayList<Thread> allThreads = new ArrayList<>();
         for(int i = 0;i<100;i++)
         {
             NumberPrinter obj = new NumberPrinter(i);
             Thread t = new Thread(obj);
+            allThreads.add(t);
             t.start();
         }
+  
+ try 
+ {
+    for(int i = 0;i<100;i++)
+    allThreads.get(i).join();
+    System.out.println("END");
+ }
+ catch(Exception ex)
+ {
+     System.out.println(ex);
+ }
+ 
+
+        
     }
 }
